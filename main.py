b4d5343756c6211436a60cb68f8f512699ee7d57
bvthxry <93049912+xxCloudd@users.noreply.github.com>
2022-07-12 15:21:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import requests
+import keyboard as k
 import time
 import random
 import threading
 import os
 
-multithreading = input("Multithreading? Y/N -> ").upper()
-length = int(input("Length -> "))
-letters = input("Letters? Y/N -> ").upper()
-numbers = input("Numbers? Y/N -> ").upper()
-underscore = input("Underscore? Y/N -> ").upper()
-lowerOrUppercase = input("Uppercase/Lowercase? U/L -> ").upper()
+print("made by bvthxry, press . to stop")
+print()
+multithreading = input("multithreading (y/n): ").upper()
+length = input("length (5-20): ")
+letters = input("letters (y/n): ").upper()
+numbers = input("numbers (y/n): ").upper()
+underscore = input("underscore (y/n): ").upper()
+lowerOrUppercase = input("uppercase/lowercase (u/l): ").upper()
 
+running = True
+
+def onPress(key):
+    global running
+    if key.name == ".":
+        running = False
+
+k.on_press(onPress)
 
 os.system("cls")
 
@@ -30,11 +41,21 @@ uCharacters = Characters
 if underscore == "Y":
     uCharacters = uCharacters + "_"
 
-if not length or length < 5:
+if type(length) != "int" or (type(length) == "int" and int(length) < 5):
     length = 5
-elif length > 20:
+elif (type(length) == "int" and int(length) > 20):
     length = 20
+    
 
+cached_users = []
+
+def isCached(Str):
+    global cached_users
+    if not Str in cached_users:
+        cached_users.append(Str)
+        return False
+    else:
+        return True
 
 def is_filtered(str):
     for swear_word in ["FCK", "COCK", "DICK", "PENIS", "FUCK", "SHIT", "CUM", "ASS", "SEX", "ROBUX", "BUX", "MILF",
@@ -55,34 +76,50 @@ def getRandomStr():
 
     newString = newString + Characters[random.randint(1, len(Characters)) - 1]
 
-    if is_filtered(newString):
+    if is_filtered(newString) or isCached(newString):
         getRandomStr()
     else:
         return newString
 
 
-def getUser():
-    theStr = getRandomStr()
-    req = requests.post("https://users.roblox.com/v1/usernames/users", {'usernames': [theStr]})
+def getUsers():
+    users = []
+    
+    for _ in range(100):
+        users.append(getRandomStr())
+    
+    req = requests.post("https://users.roblox.com/v1/usernames/users", {'usernames': users, 'excludeBannedUsers': True})
 
     if req.status_code == 200:
-        load = req.json()
+        loaded = req.json()
 
-        if len(load["data"]) == 0:
-            return theStr
-        else:
-            return False
+        for user in loaded["data"]:
+            if user["name"].lower() == user["requestedUsername"].lower(): # exists
+                users.remove(user["requestedUsername"])
 
+
+    return users
+
+iresults = 1
 
 def Do():
-    result = getUser()
-    if result:
-        print(result)
+    global iresults
+    
+    results = getUsers()
+
+    if results:
+        for user in results:
+            if user != None:
+                print(str(iresults)+":"+user)
+                iresults += 1
 
 
-while True:
+while running:
     if multithreading == "Y":
         threading.Thread(target=Do).start()
     else:
         Do()
     time.sleep(.1)
+
+print("\nstopped")
+os.system("pause")
